3a46ee49b2b59e977b604e0e2ff60fc12d2a203b
Profsain <profsaincrown@gmail.com>
2022-07-26 07:55:30 +0100

    fix linters errors

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,31 +1,29 @@
-import React from 'react'
+import React from 'react';
 import '../styles/MyProfile.css';
 
-const MyProfile = () => {
-  return (
-    <div className="Profile-container">
-      <h2>My Profile</h2>
-      <div className="Section">
-        <div className="Missions">
-          <h4>My Missions</h4>
-          <div className="Content">
-            <p>Telstar</p>
-            <p>SES</p>
-            <p>AsiaSet</p>
-            <p>ABS</p>
-          </div>
+const MyProfile = () => (
+  <div className="Profile-container">
+    <h2>My Profile</h2>
+    <div className="Section">
+      <div className="Missions">
+        <h4>My Missions</h4>
+        <div className="Content">
+          <p>Telstar</p>
+          <p>SES</p>
+          <p>AsiaSet</p>
+          <p>ABS</p>
         </div>
-        <div className="Rockets">
-            <h4>My Rockets</h4>
-          <div className="Content">
-            <p>Falcon 9</p>
-            <p>Falcon Heavy</p>
-            <p>Star Ship</p>
-          </div>
+      </div>
+      <div className="Rockets">
+        <h4>My Rockets</h4>
+        <div className="Content">
+          <p>Falcon 9</p>
+          <p>Falcon Heavy</p>
+          <p>Star Ship</p>
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default MyProfile;
