c1b0258c6260e4c2b3d679e055acbbaa98defb07
Profsain <profsaincrown@gmail.com>
2022-07-26 17:03:03 +0100

    fix linters error

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -2,36 +2,36 @@ import React from 'react';
 import RocketCards from './RocketCards';
 
 const Rockets = () => (
-    <div>
-      <RocketCards
-        image="r1.jpg"
-        title="AsiaSat"
-        description="The Rockets section displays a list of all available SpaceX rockets. 
-        Users can book each rocket by clicking the reservation button or cancel the previously made booking. 
+  <div>
+    <RocketCards
+      image="r1.jpg"
+      title="AsiaSat"
+      description="The Rockets section displays a list of all available SpaceX rockets.
+        Users can book each rocket by clicking the reservation button or cancel the previously made booking.
         The same layout is used to form the Dragons section"
-      />
-      <RocketCards
-        image="r2.jpg"
-        title="Nigeria SpaceSat"
-        description="The Rockets section displays a list of all available SpaceX rockets. 
-        Users can book each rocket by clicking the reservation button or cancel the previously made booking. 
+    />
+    <RocketCards
+      image="r2.jpg"
+      title="Nigeria SpaceSat"
+      description="The Rockets section displays a list of all available SpaceX rockets.
+        Users can book each rocket by clicking the reservation button or cancel the previously made booking.
         The same layout is used to form the Dragons section"
-      />
-      <RocketCards
-        image="r4.jpg"
-        title="Saudi PeaceSat"
-        description="The Rockets section displays a list of all available SpaceX rockets. 
-        Users can book each rocket by clicking the reservation button or cancel the previously made booking. 
+    />
+    <RocketCards
+      image="r4.jpg"
+      title="Saudi PeaceSat"
+      description="The Rockets section displays a list of all available SpaceX rockets.
+        Users can book each rocket by clicking the reservation button or cancel the previously made booking.
         The same layout is used to form the Dragons section"
-      />
-      <RocketCards
-        image="r1.jpg"
-        title="Star Japan"
-        description="The Rockets section displays a list of all available SpaceX rockets. 
-        Users can book each rocket by clicking the reservation button or cancel the previously made booking. 
+    />
+    <RocketCards
+      image="r1.jpg"
+      title="Star Japan"
+      description="The Rockets section displays a list of all available SpaceX rockets.
+        Users can book each rocket by clicking the reservation button or cancel the previously made booking.
         The same layout is used to form the Dragons section"
-      />
-    </div>
-  );
+    />
+  </div>
+);
 
 export default Rockets;
