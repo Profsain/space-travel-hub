e6e39e21387065a106a169b8be7a953b88a03541
brainconnect93 <brainconnect0@gmail.com>
2022-07-26 21:05:22 +0100

    Add dom to the dependencies and add the nav-head

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import planetImg from './public/planet2.png'
+import planetImg from './public/planet2.png';
+
+const Navbar = () => {
+  const urls = [
+    {
+      id: 1,
+      url: '/',
+      text: 'Rocket',
+    },
+    {
+      id: 2,
+      url: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      url: '/my profile',
+      text: 'My Profile',
+    },
+  ];
+
+  return (
+    <nav className="navbar">
+      <img src={planetImg} className="PlantImg" alt="Planet-img" />
+      <h1 className="nav-head">Space Travellers' Hub</h1>
+    </nav>
+  )
+}
