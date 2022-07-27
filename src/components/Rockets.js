4637c7442e2e3e53b095cddde061b9810d4bd7f5
brainconnect93 <brainconnect0@gmail.com>
2022-07-27 11:34:47 +0100

    Final solution

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import RocketCards from './RocketCards';
-import fetchRocketsData from '../redux/rockets/fetchRocketsData'
+import fetchRocketsData from '../redux/rockets/fetchRocketsData';
 
 const Rockets = () => {
   const dispatch = useDispatch();
