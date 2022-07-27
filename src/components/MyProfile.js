82faa89af3778aa091e91ab0652ce87c1f01004e
Profsain <profsaincrown@gmail.com>
2022-07-27 18:33:34 +0100

    Display reserved rocket in my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import '../styles/MyProfile.css';
 
-const MyProfile = () => (
-  <div className="Profile-container">
-    <h2>My Profile</h2>
-    <div className="Section">
-      <div className="Missions">
-        <h4>My Missions</h4>
-        <div className="Content">
-          <p>Telstar</p>
-          <p>SES</p>
-          <p>AsiaSet</p>
-          <p>ABS</p>
+const MyProfile = () => {
+  const rocketsData = useSelector((state) => state.rockets.rockets);
+  const reservedList = rocketsData.filter((rocket) => rocket.reserved === true);
+  return (
+    <div className="Profile-container">
+      <h2>My Profile</h2>
+      <div className="Section">
+        <div className="Missions">
+          <h4>My Missions</h4>
+          <div className="Content">
+            <p>Telstar</p>
+            <p>SES</p>
+            <p>AsiaSet</p>
+            <p>ABS</p>
+          </div>
         </div>
-      </div>
-      <div className="Rockets">
-        <h4>My Rockets</h4>
-        <div className="Content">
-          <p>Falcon 9</p>
-          <p>Falcon Heavy</p>
-          <p>Star Ship</p>
+        <div className="Rockets">
+          <h4>My Rockets</h4>
+          <div className="Content">
+            {
+              !reservedList.length
+              ? <p>No reservation!</p>
+              : reservedList.map((rocket) => <p key={rocket.id}>{rocket.rocket_name}</p>)
+            }
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default MyProfile;
