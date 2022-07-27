b2daf9f4776881182bf7a9ab04c72bab1b1ae43c
brainconnect93 <brainconnect0@gmail.com>
2022-07-27 18:28:19 +0100

    Add the missionview link to the app.js file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MyProfile from './components/MyProfile';
 import Rockets from './components/Rockets';
 import Navbar from './components/NavBar';
+import Missionview from './components/MissionView';
 
 const App = () => (
   <>
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/missions" element={<Missionview />} />
         <Route path="/myprofile" element={<MyProfile />} />
         <Route path="/" element={<Rockets />} />
       </Routes>
