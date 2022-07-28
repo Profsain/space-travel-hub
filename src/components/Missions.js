21d5a1b3944b9f32a83a1f146d0d956d084a4562
brainconnect93 <brainconnect0@gmail.com>
2022-07-28 02:49:01 +0100

    fix linters

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,40 +1,13 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// const Missions = (props) => {
-//   const { mission } = props;
-//   const { mission_id, mission_item, description } = mission;
-
-//   return (
-//     <tr>
-//       <td className="tab-data">{ mission_item }</td>
-//       <td>{ description }</td>
-//       <td>
-//         <button type="button" className="btn">Not a member</button>
-//       </td>
-//       <td>
-//         <button type="button" id={mission_id} className="btn">Join Mission</button>
-//       </td>
-//     </tr>
-//   );
-// };
-
-// Missions.propTypes = {
-//   mission: PropTypes.instanceOf(Object).isRequired,
-// };
-
-// export default Missions;
-
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
-import { useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Table from "react-bootstrap/Table";
-import Badge from "react-bootstrap/Badge";
-import Button from "react-bootstrap/Button";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchMissions } from "../redux/missions/fetchMissionData";
+import { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/fetchMissionData';
 
 const Missions = () => {
   const dispatch = useDispatch();
