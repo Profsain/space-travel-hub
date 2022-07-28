b147dde18f9fc74158d56757ff748d5c3cea9d54
brainconnect93 <brainconnect0@gmail.com>
2022-07-28 03:17:30 +0100

    Add the leave and join mission functionality

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable */
+import { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/fetchMissionData';
 
-const Missions = (props) => {
-  const { mission } = props;
-  const { missionId, missionItem, description } = mission;
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    if (!missions.length) {
+      dispatch(fetchMissions);
+    }
+  }, []);
+
+  const joinMissionHandle = (id) => dispatch(joinMission(id));
+  const leaveMissionHandler = (id) => dispatch(leaveMission(id));
 
   return (
-    <tr>
-      <td className="tab-data">{ missionItem }</td>
-      <td>{ description }</td>
-      <td>
-        <button type="button" className="btn">Not a member</button>
-      </td>
-      <td>
-        <button type="button" id={missionId} className="btn">Join Mission</button>
-      </td>
-    </tr>
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{"  "}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map(({
+            id, name, description, reserved,
+          }) => (
+            <tr key={id}>
+              <td>
+                <p className="fw-bold">{name}</p>
+              </td>
+              <td>
+                <p>{description}</p>
+              </td>
+              <td className="align-middle">
+                {reserved && <Badge bg="info">Active Member</Badge>}
+                {!reserved && <Badge bg="secondary">NOT A MEMBER</Badge>}
+              </td>
+              <td className="col-2 align-middle text-center">
+                {reserved && (
+                <Button 
+                  size="sm" 
+                  variant="outline-danger"
+                  onClick={() => leaveMissionHandler(id)}>
+                  Leave Mission
+                </Button>
+                )}
+                {!reserved && (
+                <Button 
+                  size="sm" 
+                  variant="outline-secondary"
+                  onClick={() => joinMissionHandle(id)}>
+                  Join Mission
+                </Button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
-};
-
-Missions.propTypes = {
-  mission: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Missions;
