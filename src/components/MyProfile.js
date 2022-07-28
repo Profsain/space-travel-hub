9100b094253859e714330497f72a464067f20cea
Profsain <profsaincrown@gmail.com>
2022-07-28 15:20:04 +0100

    fix key errors

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,37 +1,33 @@
-/* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import { ListGroup } from 'react-bootstrap';
 import '../styles/MyProfile.css';
 
 const MyProfile = () => {
   const rocketsData = useSelector((state) => state.rockets.rockets);
   const reservedList = rocketsData.filter((rocket) => rocket.reserved === true);
-  // const missionList = useSelector((state) => state.missions.mission);
-  // console.log('missionList', missionList);
   const missionProfile = useSelector((state) => [
     {
       MissionTitle: "My Mission",
       data: state.missions.filter((mission) => mission.reserved),
     },
   ])
-  console.log(missionProfile[0].data);
 
   return (
     <div className="Profile-container">
       <h2>My Profile</h2>
       <div className="Section">
-        {missionProfile.map(({ MissionTitle, data}) => (
+        {missionProfile.map(({ MissionTitle, data }) => (
           <div className="Missions">
             <h4>{MissionTitle}</h4>
-              <div className="Content">
-                {data.map((item) => (
-                  <p key={item.id}>{item.name}</p>
-                ))}
-
-              </div>
+            <div className="Content">
+              {
+                !data.length
+                  ? <p>No Missions</p>
+                  : data.map((item) => <p key={item.id}>{item.name}</p>)
+              }
             </div>
-          ))}
+          </div>
+        ))}
         <div className="Rockets">
           <h4>My Rockets</h4>
           <div className="Content">
